e1436dd94a4f8f11261008d140ebf42fb43dc139
buctor41 <buctor41@gmail.com>
2020-11-09 20:22:57 +0800

    20201109

--- atcoder/ABC182/C.cpp
+++ atcoder/ABC182/C.cpp
@@ -1,43 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
+int lg[3];
 int main()
 {
-    int r = 0,a = 0,b = 0;
+    int r = 0;
     string s;
     cin >> s;
     int n = s.size();
-    for(int i = 0; i < s.size(); i++){
+    for(int i = 0; i < n; i++){
         int v = s[i] - '0';
-        v = v % 3;
-        r = (r+v) % 3;
-        if(v==1) a++;
-        else b++;
+        r += v;
+        r %= 3;
+        v %= 3;
+        lg[v]++;
     }
     if(r==0){
-        cout << 0 << endl;
+        cout << "0" << endl;
         return 0;
     }
-    if(r==1){
-        if(a>=1 && n > 1){
-            cout << 1 << endl;
-            return 0;
-        }
-        if(b>=2 && n > 2){
-            cout << 2 << endl;
-            return 0;
-        }
-        cout << -1 << endl;
+    if(n>=3){
+        if(lg[r]>=1) cout << "1" << endl;
+        else cout << "2" << endl;
         return 0;
     }else{
-        if(b>=1 && n > 1){
-            cout <<1 << endl;
+        if(n==1){
+            cout << "-1" <<endl;
             return 0;
         }
-        if(a>=2 && n > 2){
-            cout <<2 <<endl;
-            return 0;
-        }
-        cout << -1 << endl;
-        return 0;
+        if(lg[r]>=1) cout << "1" << endl;
+        else cout << "-1" << endl;
     }
+    return 0;
 }
