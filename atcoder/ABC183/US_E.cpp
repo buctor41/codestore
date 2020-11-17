a99794f471f04eef4c1a375c00d88b5b2fecb6ed
buctor41 <buctor41@gmail.com>
2020-11-17 21:49:25 +0800

    20201117

---
+++ atcoder/ABC183/US_E.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair <int, int> pii;
+#define int ll
+
+#define pb push_back
+#define F first
+#define S second
+#define _sz(x) ((int)x.size())
+
+#define fastio ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
